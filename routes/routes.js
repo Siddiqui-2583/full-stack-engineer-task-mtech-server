986763529ff16d352f0ada6e1d47e4dfe4123870
Siddiqui-2583 <nurnaby83@gmail.com>
2021-06-26 08:11:20 +0600

    root route updated!

--- routes/routes.js
+++ routes/routes.js
@@ -2,9 +2,9 @@ import express from 'express'
 import { insertProduct,placeOrder,getAllProducts,getAllOrders } from "../controllers/controller.js";
 
 const router = express.Router();
-router.get("/", () => {
- console.log('Hello node') 
-})
+router.get("/", (req, res) => {
+  res.json("Hello Node");
+});
 router.post("/insert",insertProduct)
 router.get("/all-products", getAllProducts)
 router.get("/all-orders", getAllOrders);
